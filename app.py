76b52690707c90b6332034ced4a04d579e95c875
IsaacVera10 <isaac.vera@utec.edu.pe>
2023-06-19 00:55:58 -0500

    Simplificacion de condicionales

--- app.py
+++ app.py
@@ -7,34 +7,36 @@ class CalculaGanador:
         with open(archivo, 'r') as csvfile:
             next(csvfile)
             data_votos = csv.reader(csvfile)
-            for fila in data_votos: 
+            for fila in data_votos:
                 data.append(fila)
         return data
-    
-    def isValidDNI(self, dni):  #División de métodos
+
+    def isValidDNI(self, dni):#División de métodos
         return len(dni) == 8 and dni.isdigit()
 
     def calcularganador(self, data):
         votosxcandidato = {}
         total_votos_validos = 0
+
         for fila in data:
             region, provincia, distrito, dni, candidato, es_valido = fila #Renombrar variables
-            if self.isValidDNI(dni) and es_valido == '1': #Simplificación de condicionales
+
+            if self.isValidDNI(dni) and es_valido == '1':#Simplificación de condicionales
                 total_votos_validos += 1
-                if candidato not in votosxcandidato:
-                    votosxcandidato[candidato] = 0
-                votosxcandidato[candidato] += 1
+
+                votosxcandidato[candidato] = votosxcandidato.get(candidato, 0) + 1 #Simplificación de condicionales, uso de dict.get()
 
         porcentaje_ganador = 0.5 * total_votos_validos
         ganador = None
         segunda_vuelta = []
         no_ganador = []
+
         for candidato, votos in votosxcandidato.items():
             if votos > porcentaje_ganador:
                 ganador = candidato
             elif votos == porcentaje_ganador:
                 segunda_vuelta.append(candidato)
-            elif votos < porcentaje_ganador:
+            else:                   #Simplificación de condicionales: uso de else en vez de elif que cubría casos contrarios.
                 no_ganador.append(candidato)
 
         if ganador:
@@ -46,6 +48,7 @@ class CalculaGanador:
         else:
             no_ganador_ordenado = sorted(no_ganador, key=lambda candidato: votosxcandidato[candidato], reverse=True)
             return no_ganador_ordenado[:2]
+
 
 c = CalculaGanador()
 datatest = [
