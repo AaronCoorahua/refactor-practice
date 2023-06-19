fac4af3c688a00ad25441dde819f384924354f00
Pedro Mori <pmori1142@gmail.com>
2023-06-19 01:13:05 -0500

    Another fix

--- app.py
+++ app.py
@@ -1,73 +1,48 @@
 import csv
 
-class CalculaGanador:
-
+class Datos(): #División de clases
+    def __init__(self, data):
+        self.data = data
+    
     def leerdatos(self, archivo):
-        data = []
         with open(archivo, 'r') as csvfile:
             next(csvfile)
             data_votos = csv.reader(csvfile)
-            for fila in data_votos:
-                data.append(fila)
-        return data
+            for fila in data_votos: 
+                self.data.append(fila)
+    
 
-    def isValidDNI(self, dni):# 1) Extraccion de métodos
-        return len(dni) == 8 and dni.isdigit()
+class CalculaGanador:
 
-    def calcular_ganador(self, data):
+    def isValidDNI(self,dni): #División de métodos
+        return (len(dni) == 8 and dni.isdigit())
+
+    def calcularganador(self, data):
         votosxcandidato = {}
         total_votos_validos = 0
-
         for fila in data:
-            region, provincia, distrito, dni, candidato, es_valido = fila # 2) Renombrar variables
-            if self.isValidDNI(dni) and es_valido == '1':  # 3) Simplificación de condicionales
+            region, provincia, distrito, dni, candidato, es_valido = fila #Renombrar variables
+            if self.isValidDNI(dni) and es_valido == '1': #Simplificación de condicionales
                 total_votos_validos += 1
-                votosxcandidato[candidato] = votosxcandidato.get(candidato, 0) + 1 #Simplificación de condicionales, uso de dict.get()
-                 # 4) Eliminacion de codigo duplicado
+                if candidato not in votosxcandidato:
+                    votosxcandidato[candidato] = 0
+                votosxcandidato[candidato] += 1
 
-        return self.obtener_resultado(total_votos_validos, votosxcandidato)
-    
-    # 5)División de metodos
-    def obtener_resultado(self, total_votos_validos, votosxcandidato):
         porcentaje_ganador = 0.5 * total_votos_validos
         ganador = None
-        segunda_vuelta = []
-        no_ganador = []
-
-
+        segunda_vuelta = {}
+        empate = []
         for candidato, votos in votosxcandidato.items():
             if votos > porcentaje_ganador:
                 ganador = candidato
-            elif votos == porcentaje_ganador:
-                segunda_vuelta.append(candidato)
-            else:                   #Simplificación de condicionales: uso de else en vez de elif que cubría casos contrarios.
-                no_ganador.append(candidato)
+            if votos == porcentaje_ganador:
+                empate.append(candidato)
+            segunda_vuelta[candidato] = votos
 
-        return self.obtener_resultado_final(ganador, segunda_vuelta, no_ganador, votosxcandidato)
-
-    def obtener_resultado_final(self, ganador, segunda_vuelta, no_ganador, votosxcandidato):
         if ganador:
             return [ganador]
-        elif len(segunda_vuelta) >= 2:
-            return segunda_vuelta[:1]
-        elif len(segunda_vuelta) == 1:
-            return segunda_vuelta + [list(votosxcandidato.keys())[0]]
+        elif len(empate) > 0:
+            return [empate[0]]
         else:
-            no_ganador_ordenado = sorted(no_ganador, key=lambda candidato: votosxcandidato[candidato], reverse=True)
-            return no_ganador_ordenado[:2]
-
-
-c = CalculaGanador()
-datatest = [
-    ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '0'],
-    ['Áncash', 'Asunción', 'Acochaca', '57533597', 'Eddie Hinesley', '1'],
-    ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '0'],
-    ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '0'],
-    ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '1'],
-    ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '0'],
-]
-print(c.calcular_ganador(datatest))
-
-c = CalculaGanador()
-datos = c.leerdatos("0204.csv")
-print(c.calcular_ganador(datos))
+            candidatos_ord_votos = dict(sorted(segunda_vuelta.items(), key=lambda x:x[1], reverse=True))
+            return [candidato for candidato in list(candidatos_ord_votos)[:2]]
--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from app import CalculaGanador
+from app import CalculaGanador,Datos
 
 class CalculaGanadorTest(unittest.TestCase):
     def setUp(self):
@@ -12,7 +12,7 @@ class CalculaGanadorTest(unittest.TestCase):
             ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '0'],
             ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '0']
         ]
-        resultado = self.calculador.calcular_ganador(data)
+        resultado = self.calculador.calcularganador(data)
         self.assertEqual(resultado, ['Eddie Hinesley'])
 
     def test_empate(self):
@@ -22,7 +22,7 @@ class CalculaGanadorTest(unittest.TestCase):
             ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '1'],
             ['Áncash', 'Asunción', 'Acochaca', '57533597', 'Eddie Hinesley', '1'],        
         ]
-        resultado = self.calculador.calcular_ganador(data)
+        resultado = self.calculador.calcularganador(data)
         self.assertEqual(resultado, ['Aundrea Grace'])
 
     def test_segunda_vuelta(self):
@@ -36,8 +36,16 @@ class CalculaGanadorTest(unittest.TestCase):
             ['Áncash', 'Asunción', 'Acochaca', '86777546', 'Aundrea Grace', '1'],  
             ['Áncash', 'Asunción', 'Acochaca', '86772655', 'Aundrea Grace', '1'],     
         ]
-        resultado = self.calculador.calcular_ganador(data)
+        resultado = self.calculador.calcularganador(data)
         self.assertEqual(resultado, ['Eddie Hinesley','Aundrea Grace'])
+
+    def test_archivo(self):
+        d = Datos([])
+        d.leerdatos('0204.csv')
+
+        resultado = self.calculador.calcularganador(d.data)
+        self.assertEqual(resultado, ['Dennis Reyna', 'Aundrea Grace'])
+
 
 if __name__ == '__main__':
     unittest.main()
